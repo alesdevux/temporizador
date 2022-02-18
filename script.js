b6041eb96b37f7ca947ff480f56bea5b9d4e0206
Ales <aleswebgit@gmail.com>
2022-02-18 11:54:47 +0100

    prepare interaccion with DOM

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 let stateTimer = false;
 
+const domMin = document.getElementById('minutes');
+const domSec = document.getElementById('seconds');
+
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+const minutes = 3;
+const seconds = 0;
+
+if (minutes < 10) {
+  domMin.innerHTML = `0${minutes}`;
+} else {
+  domMin.innerHTML = minutes;
+}
+
+if (seconds < 10) {
+  domSec.innerHTML = `0${seconds}`;
+} else {
+  domSec.innerHTML = seconds;
+}
